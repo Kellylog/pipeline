ff8883da51763cdb5c853071c1c80644e42ff387
kellylog <kellylogblog@gmail.com>
2016-06-07 11:36:02 +0000

    12factor gem and complie assets

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pipeline::Application.config.secret_key_base = '75cc747b0081cf961f4db7637052167cc30ebea1c49d9e7ac053f0a47ffae7a682f65f3c7238577c90f9e81a64d1239b117f3c92c41310cca3eb472e97e93c4f'
